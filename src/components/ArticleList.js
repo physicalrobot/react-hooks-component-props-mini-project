8e3b5682b054e75e0ba9dd1866eb18ae309a2fd6
Vikalp Malhotra <kimboho@gmail.com>
2022-01-11 20:39:33 -0500

    done

---
+++ src/components/ArticleList.js
@@ -0,0 +1,28 @@
+import blogData from "../data/blog";
+import React from 'react';
+import Article from './Article'
+
+function ArticleList({ posts }) {
+
+
+
+    const articles = posts.map((post) => (
+
+        <Article
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            preview={post.preview} />
+
+
+    ));
+
+
+
+
+    return <main>{articles}</main>;
+
+
+}
+
+export default ArticleList;
